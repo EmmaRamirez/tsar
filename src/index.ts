f7b58a24cd061c48be00b5e210cf8bb9c9c4b06f
Emma Ramirez <ramirez.emma.g@gmail.com>
2017-04-10 16:21:36 -0600

    feat: add String Enum type
    This adds a type that functions as a String Enum, taking an array of strings as its parameter.

--- src/index.ts
+++ src/index.ts
@@ -3,3 +3,5 @@ export { Partial } from './Partial';
 export { Readonly } from './Readonly';
 export { Queue } from './Queue';
 export { Action } from './Action';
+export { StringEnum } from './StringEnum';
+export { Singleton } from './Singleton';
