a9127720ef13ba810d57bbd3a7c4e26ea903d6a2
Emma Ramirez <ramirez.emma.g@gmail.com>
2017-04-10 17:34:28 -0600

    feat: add Maybe type
    Maybe<T> is a type that lazily evaluates its contents, which can either be Just<T> or Nothing<T>.
    This implementation borrows from node implemenations of Maybe, which also borrow from Haskell's
    Maybe type.

---
+++ src/Maybe.ts
@@ -0,0 +1,65 @@
+type Just<T> = (val: T) => T;
+type Nothing = () => void;
+
+const Nothing = () => { };
+Nothing.prototype.isJust = () => false;
+Nothing.prototype.isNothing = () => true;
+
+const Just = (val: any) => {
+  this.val = val;
+  return this;
+};
+Just.prototype.isJust = () => true;
+Just.prototype.isNothing = () => false;
+
+interface Maybe<T> {
+  val: T;
+  _evaluate: () => Nothing | Just<T>;
+}
+
+class Maybe<T> implements Maybe<T> {
+  constructor(val: T) {
+    if (val instanceof Maybe) return val;
+    if (this instanceof Maybe) {
+      this.val = val;
+    } else {
+      return new Maybe<T>(val);
+    }
+  }
+
+  public evaluate():Nothing | Just<T> {
+    const value = typeof this.val === 'function' ?  this.val() : this.val;
+
+    return (value === undefined || value === null)
+      ? value
+      : value instanceof Nothing || value instanceof Just
+        ? Nothing()
+        : Just(value);
+  }
+
+  public value():Nothing | Just<T> | T {
+    const maybe = this.evaluate();
+    if (maybe instanceof Nothing) {
+      return maybe;
+    } else if (maybe instanceof Just) {
+      return maybe.value;
+    } else {
+      return maybe;
+    }
+  }
+
+  public memoize() {
+    this._evaluate = this.evaluate;
+    this.evaluate = function () {
+      return this.memoVal || (this.memoVal = this._evaluate());
+    }.bind(this);
+  }
+
+  public isJust():boolean {
+    return this.evaluate().isJust();
+  }
+
+  public isNothing():boolean {
+    return this.evaluate().isNothing();
+  }
+}
